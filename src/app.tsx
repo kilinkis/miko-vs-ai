f86402c75e8b317db528a6441c65f9715c74adc0
Juan Manuel Incaurgarat <juanofx@gmail.com>
2023-03-23 12:23:22 +0100

    Some cleanup and added consts

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,42 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'preact/hooks'
 import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+const data = [
+    {
+        companyName: 'Acme LLC',
+    },
+    {
+        companyName: 'Apple llc',
+    },
+    {
+        companyName: 'Wayne industries',
+    },
+    {
+        companyName: 'Pleo limited liability company',
+    },
+]
 
-  return (
-    <>
-      <h1>MIKO vs AI</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+const queries = {
+    llc: 'how many "llc" are there in the dataset (case insensitive)? I want a number. Consider "limited liability company" as "llc"',
+}
+
+export function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div class="grid gap-12">
+            <h1 class="text-5xl">MIKO vs AI</h1>
+            <div>
+                <button
+                    class="border border-indigo-700 rounded-lg py-2 px-4"
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/app.tsx</code> and save to test HMR
+                </p>
+            </div>
+        </div>
+    )
 }
